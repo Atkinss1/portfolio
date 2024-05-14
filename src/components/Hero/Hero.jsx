05d56642b8b68f882bc0ab79c34ede84021b7134
Atkinss <jordanatkins@live.com>
2024-05-13 20:09:36 -0600

    add: general layout attempt

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,30 @@
-import './hero.css'
+import './hero.css';
 
 const Hero = () => {
-  return ( <div className="main_hero_wrapper">
-    <div className="name_container">Jordan</div>
-  </div> );
-}
- 
+  return (
+    <div className="hero_wrapper">
+      <div className="container">
+        <img className="profile_thumbnail" src='images/user-2.jpg'/>
+          <div className="profile_name">
+            <h1>
+              <span>
+                - Jordan Atkins -
+              </span>
+            </h1>
+          </div>
+            <h3>
+              <span>Web Developer</span>
+            </h3>
+          <div className="social_media_wrapper">
+            <ul className="social_media_icons">
+              <li><a href="">GitHub</a></li>
+              <li><a href="">LinkedIn</a></li>
+            </ul>
+          </div>
+          
+      </div>
+    </div>
+  );
+};
+
 export default Hero;
