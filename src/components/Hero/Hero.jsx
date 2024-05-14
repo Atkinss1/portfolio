1646e91abc2c74e7a73fb3b253a01ef138425546
Atkinss <jordanatkins@live.com>
2024-05-13 21:36:35 -0600

    add: fontawesome script, social media icons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,13 +12,26 @@ const Hero = () => {
               </span>
             </h1>
           </div>
-            <h3 className='web_dev'>
+            <h3 className='web_dev_title'>
               <span>Full-Stack Web Developer</span>
             </h3>
           <div className="social_media_wrapper">
             <ul className="social_media_icons">
-              <li><a href="">GitHub</a></li>
-              <li><a href="">LinkedIn</a></li>
+              <li>
+                <a href="https://github.com/Atkinss1">
+                  <i className="fa-brands fa-github fa-2xl"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/atkins-jordan/">
+                  <i className="fa-brands fa-linkedin fa-2xl"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/atkinsz/">
+                  <i class="fa-brands fa-instagram fa-2xl"></i>
+                </a>
+              </li>
             </ul>
           </div>
           
