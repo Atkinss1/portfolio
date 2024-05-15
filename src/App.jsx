c6597e37bd1f94b272d1bb75e0a7b789852632b6
Atkinss <jordanatkins@live.com>
2024-05-14 22:09:56 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import Hero from "./components/Hero/Hero";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Hero />
-    </BrowserRouter>
     </>
   )
 }
