29df5837fe6c658e3b1033a251429c0643cd5af3
Atkinss <jordanatkins@live.com>
2024-05-14 18:51:57 -0600

    add: css styling, About me section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -40,7 +40,40 @@ const Hero = () => {
 
       <div className="about_me_wrapper">
         <div className="about_me_container">
-          <h1>TESTING</h1>
+          <div className="about_me">
+            <h2>About Me</h2>
+          </div>
+          <div className="details_container">
+            <ul className="about_me_details">
+              <li>Full Name: Jordan Atkins</li>
+              <li>Phone: (780) 966-7231</li>
+              <li>Email: Jordanatkins@live.com</li>
+              <li>Stomping Grounds: Edmonton, Alberta</li>
+            </ul>
+            <div className="about_me_bio">
+              <h2>Hey There!</h2>
+              <p>
+                I’m Jordan Atkins, a full stack developer with 14 years of experience in the oil and gas industry. My career has been defined by a passion for creating seamless, user-friendly websites and collaborating on innovative projects. I’m also an avid enthusiast of the auto and gaming industries, constantly seeking to blend my technical expertise with these personal interests. As I continue to expand my skills, my goal is to build amazing digital experiences and contribute to dynamic teams in delivering outstanding results.
+              </p>
+              <ul className="social_media_icons">
+                <li>
+                  <a href="https://github.com/Atkinss1">
+                    <i className="fa-brands fa-github fa-2xl"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/atkins-jordan/">
+                    <i className="fa-brands fa-linkedin fa-2xl"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/atkinsz/">
+                    <i class="fa-brands fa-instagram fa-2xl"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
