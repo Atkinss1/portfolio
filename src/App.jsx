bf281ad2b20f6798a4405126a317f815d7cd9b0b
Atkinss <jordanatkins@live.com>
2024-05-14 18:08:03 -0600

    remove NavBar, add css to hide scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Hero from "./components/Hero/Hero"
-import Navbar from "./components/NavBar/NavBar"
 
 function App() {
 
   return (
     <>
-    {/* <Navbar/> */}
     <Hero />
     </>
   )
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,41 +2,48 @@ import './hero.css';
 
 const Hero = () => {
   return (
-    <div className="hero_wrapper">
-      <div className="container">
-        <img className="profile_thumbnail" src='images/user-2.jpg'/>
-          <div className="profile_name">
-            <h1>
-              <span>
-                - Jordan Atkins -
-              </span>
-            </h1>
-          </div>
-            <h3 className='web_dev_title'>
-              <span>Full-Stack Web Developer</span>
-            </h3>
-          <div className="social_media_wrapper">
-            <ul className="social_media_icons">
-              <li>
-                <a href="https://github.com/Atkinss1">
-                  <i className="fa-brands fa-github fa-2xl"></i>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/atkins-jordan/">
-                  <i className="fa-brands fa-linkedin fa-2xl"></i>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/atkinsz/">
-                  <i class="fa-brands fa-instagram fa-2xl"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-          
+    <>
+      <div className="hero_wrapper">
+        <div className="container">
+          <img className="profile_thumbnail" src='images/user-2.jpg'/>
+            <div className="profile_name">
+              <h1>
+                <span>
+                  - Jordan Atkins -
+                </span>
+              </h1>
+            </div>
+              <h3 className='web_dev_title'>
+                <span>Full-Stack Web Developer</span>
+              </h3>
+            <div className="social_media_wrapper">
+              <ul className="social_media_icons">
+                <li>
+                  <a href="https://github.com/Atkinss1">
+                    <i className="fa-brands fa-github fa-2xl"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/atkins-jordan/">
+                    <i className="fa-brands fa-linkedin fa-2xl"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/atkinsz/">
+                    <i class="fa-brands fa-instagram fa-2xl"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+        </div>
       </div>
-    </div>
+
+      <div className="about_me_wrapper">
+        <div className="about_me_container">
+          <h1>TESTING</h1>
+        </div>
+      </div>
+    </>
   );
 };
 
