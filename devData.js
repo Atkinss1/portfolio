feb1bf0b94f49fe4498f267e95c7544d8475a8b3
Atkinss <jordanatkins@live.com>
2024-05-15 22:39:25 -0600

    Updates

---
+++ devData.js
@@ -0,0 +1,12 @@
+const devData = {
+  name: "- Jordan Atkins -",
+  position: "Full-Stack Developer",
+  bio: "I`m Jordan Atkins, a junior full stack developer with 14 years of experience in the oil and gas industry as a welder. My career has been defined by a passion for solving problems, and creating things with my hands. This has lead me to a healthier future by thriving in gaining hands on experience building user-friendly websites and collaborating on innovative projects. I`m also an avid enthusiast of the auto and gaming industries, constantly seeking to blend my technical expertise with these personal interests. As I continue to expand my skills, my goal is to build amazing digital experiences and contribute to dynamic teams in delivering outstanding results.",
+  details: {name: "Full Name: Jordan Atkins",
+            phone: "Phone: (780) 966 - 7231",
+            email: "Email: Jordanatkins@live.com",
+            location: "Stomping Grounds: Edmonton, Alberta"}
+
+}
+
+export default devData;
---
+++ projectData.js
@@ -0,0 +1,28 @@
+const projectData = {
+  propBox: { name: "Prop.Box",
+             link: "https://github.com/Atkinss1/Prop-Box",
+             image: "./images/PropBox-Mockup.jpg",
+             description: "Prop.Box is a collaborative inventory and project management tool designed specifically for the 'props' department within episodic film production. It serves as a centralized hub for tracking props, scenes, episodes, and projects (shows), aiming to bring clarity and efficiency to the production process.",
+             techStack: "Tech-Stack: ReactJs, NodeJs, CSS, HTML, MaterialUI "
+            },
+  tweeter: { name: "Tweeter",
+             link: "https://github.com/Atkinss1/tweeter",
+             image: "./images/tweeterImage.png",
+             description: "Tweeter is a twitter (or X) clone with my spin on the design. It displays my useage of jQuery and CSS skills.",
+             techStack: "Tech-Stack: JS, jQuery, CSS, HTML"
+            },
+  trueRetro: { name: "True Retro",
+               link: "https://github.com/Atkinss1/Car-Dealership",
+               image: "./images/dealership_website.png",
+               description: "A platform designed for listing cars for sale, facilitating direct chat communication between buyers and sellers. It features price-based search filters and enables users to save listings to a favorites list for easy access.",
+               techStack: "Tech-Stack: JS, EJS, jQuery, CSS, HTML, Bootstrap"
+              },
+  chatApp: { name: "Chat App",
+             link: "https://github.com/Atkinss1/chatApp",
+             image: "./images/chatApp.png",
+             description: "Welcome to the Chat Application! This project is a simple real-time chat application built using Node.js, Express, and Socket.io. It provides a platform for users to engage in real-time conversations, making it an ideal project for learning and experimenting with websockets.",
+             techStack: "Tech-Stack: JS, jQuery, NodeJs, HTML, CSS"
+            }
+}
+
+export default projectData;
