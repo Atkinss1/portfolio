7040abe81828f71580b46e867cd4cdedd9312e94
Atkinss <jordanatkins@live.com>
2024-05-15 21:58:48 -0600

    add: devData information, insert devData in html

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,22 @@
 import './hero.css';
 import SocialIcons from '../SocialIcons/SocialIcons';
+import devData from '../../../public/data';
 
 const Hero = () => {
   return (
     <>
       <div className="hero_wrapper">
         <div className="container">
-          <img className="profile_thumbnail" src='images/user-2.jpg'/>
+          <img className="profile_thumbnail" src='images/user-2.jpg' alt={`${devData.name}'s Profile`}/>
             <div className="profile_name">
               <h1>
                 <span>
-                  - Jordan Atkins -
+                  {devData.name}
                 </span>
               </h1>
             </div>
               <h3 className='web_dev_title'>
-                <span>Full-Stack Web Developer</span>
+                <span>{devData.position}</span>
               </h3>
             <div className="social_media_wrapper">
               <SocialIcons/>
@@ -29,12 +30,13 @@ const Hero = () => {
             <h2>About Me</h2>
           </div>
           <div className="details_container">
-            <ul className="about_me_details">
-              <li>Full Name: Jordan Atkins</li>
-              <li>Phone: (780) 966-7231</li>
-              <li>Email: Jordanatkins@live.com</li>
-              <li>Stomping Grounds: Edmonton, Alberta</li>
-            </ul>
+            <div className="about_me_bio">
+              <h2>Hey There!</h2>
+              <p>
+                {devData.bio}
+              </p>
+              <SocialIcons/>
+            </div>
             <br/>
             <br/>
             <br/>
@@ -46,13 +48,12 @@ const Hero = () => {
             <br/>
             <br/>
             <br/>
-            <div className="about_me_bio">
-              <h2>Hey There!</h2>
-              <p>
-                I`m Jordan Atkins, a junior full stack developer with 14 years of experience in the oil and gas industry as a welder. My career has been defined by a passion for solving problems, and creating things with my hands. This has lead me to a healthier future by thriving in gaining hands on experience building user-friendly websites and collaborating on innovative projects. I`m also an avid enthusiast of the auto and gaming industries, constantly seeking to blend my technical expertise with these personal interests. As I continue to expand my skills, my goal is to build amazing digital experiences and contribute to dynamic teams in delivering outstanding results.
-              </p>
-              <SocialIcons/>
-            </div>
+            <ul className="about_me_details">
+              <li>{devData.details.name}</li>
+              <li>{devData.details.phone}</li>
+              <li>{devData.details.email}</li>
+              <li>{devData.details.location}</li>
+            </ul>
           </div>
         </div>
       </div>
