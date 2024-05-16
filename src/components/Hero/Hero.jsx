3293f96f2f20b5cb3727bac1afb6de185e47e847
Atkinss <jordanatkins@live.com>
2024-05-15 20:10:47 -0600

    add: html to bio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
             <div className="about_me_bio">
               <h2>Hey There!</h2>
               <p>
-                I`m Jordan Atkins, a full stack developer with 14 years of experience in the oil and gas industry. My career has been defined by a passion for creating seamless, user-friendly websites and collaborating on innovative projects. I`m also an avid enthusiast of the auto and gaming industries, constantly seeking to blend my technical expertise with these personal interests. As I continue to expand my skills, my goal is to build amazing digital experiences and contribute to dynamic teams in delivering outstanding results.
+                I`m Jordan Atkins, a junior full stack developer with 14 years of experience in the oil and gas industry as a welder. My career has been defined by a passion for solving problems, and creating things with my hands. This has lead me to a healthier future by thriving in gaining hands on experience building user-friendly websites and collaborating on innovative projects. I`m also an avid enthusiast of the auto and gaming industries, constantly seeking to blend my technical expertise with these personal interests. As I continue to expand my skills, my goal is to build amazing digital experiences and contribute to dynamic teams in delivering outstanding results.
               </p>
               <SocialIcons/>
             </div>
